4572a7b3755d3b71855498321bf7d5d3c4e6e6a4
S-Delowar <sayed.buet97@gmail.com>
2025-11-21 15:55:29 +0600

    replace middleware.js to proxy.js for redirects

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    proxyTimeout: 5000, // enable new proxy system
+  },
 };
 
 export default nextConfig;
