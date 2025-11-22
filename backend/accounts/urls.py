5d615313019581e60393f2dc35c8e782595da54e
S-Delowar <sayed.buet97@gmail.com>
2025-11-22 15:38:34 +0600

    fix:chat button responsive and refresh token

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from accounts.views import LoginView, RegisterView
-
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('signin/', LoginView.as_view(), name='signin'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
    ]
